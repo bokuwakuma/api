a61bd36266edd582803d3b41d939e07de95aea36
bokuwakuma <boku_wa_kuma0102@icloud.com>
2019-06-11 18:26:07 +0900

    RESTful with HATEOAS

--- src/main/java/jp/alhinc/orchestra/api/payroll/EmployeeController.java
+++ src/main/java/jp/alhinc/orchestra/api/payroll/EmployeeController.java
@@ -1,23 +1,44 @@
 package jp.alhinc.orchestra.api.payroll;
 
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.Resources;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
 @RestController
 public class EmployeeController {
 
     private final EmployeeRepository repository;
 
+    private final EmployeeResourceAssember assembler;
+
     // Injection
-    EmployeeController(EmployeeRepository repository) {
+    EmployeeController(EmployeeRepository repository,
+                       EmployeeResourceAssember assembler) {
         this.repository = repository;
+        this.assembler = assembler;
     }
 
     // Aggregate root
     @GetMapping("/employees")
-    List<Employee> all() {
-        return repository.findAll();
+    Resources<Resource<Employee>> all() {
+//        List<Resource<Employee>> employees = repository.findAll().stream()
+//                .map(employee -> new Resource<>(employee,
+//                        linkTo(methodOn(EmployeeController.class).one(employee.getId())).withSelfRel(),
+//                        linkTo(methodOn(EmployeeController.class).all()).withRel("employees")))
+//                .collect(Collectors.toList());
+        List<Resource<Employee>> employees = repository.findAll().stream()
+                .map(assembler::toResource)
+                .collect(Collectors.toList());
+
+        return new Resources<>(employees,
+                linkTo(methodOn(EmployeeController.class).all()).withSelfRel());
     }
 
     @PostMapping("/employees")
@@ -26,10 +47,19 @@ public class EmployeeController {
     }
 
     // Single item
+
+    /**
+     * Resource<T> is a generic container from Spring HATEOAS that includes not only the data but a collection of links.
+     *
+     * @param id
+     * @return
+     */
     @GetMapping("/employees/{id}")
-    Employee one(@PathVariable Long id) {
-        return repository.findById(id)
+    Resource<Employee> one(@PathVariable Long id) {
+        Employee employee = repository.findById(id)
                 .orElseThrow(() -> new EmployeeNotFoundException(id));
+
+        return assembler.toResource(employee);
     }
 
     @PutMapping("/employees/{id}")
---
+++ src/main/java/jp/alhinc/orchestra/api/payroll/EmployeeResourceAssember.java
@@ -0,0 +1,23 @@
+package jp.alhinc.orchestra.api.payroll;
+
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.ResourceAssembler;
+import org.springframework.stereotype.Component;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+
+@Component
+public class EmployeeResourceAssember implements ResourceAssembler<Employee, Resource<Employee>> {
+
+    @Override
+    public Resource<Employee> toResource(Employee employee) {
+        return new Resource<>(employee,
+                linkTo(methodOn(EmployeeController.class).one(employee.getId())).withSelfRel(),
+                linkTo(methodOn(EmployeeController.class).all()).withRel("employees"));
+    }
+
+}
+
+
+
