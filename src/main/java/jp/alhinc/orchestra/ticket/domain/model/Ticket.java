a8cf720e9a0f47cbd8ddcf8a6d648e7f7b066d69
bokuwakuma <boku_wa_kuma0102@icloud.com>
2019-06-17 17:09:40 +0900

    add JPA configuration

--- src/main/java/jp/alhinc/orchestra/ticket/domain/model/Ticket.java
+++ src/main/java/jp/alhinc/orchestra/ticket/domain/model/Ticket.java
@@ -2,18 +2,33 @@ package jp.alhinc.orchestra.ticket.domain.model;
 
 import lombok.Data;
 
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.io.Serializable;
 import java.util.Date;
 
 @Data
+@Entity
+@Table(name = "ticket")
 public class Ticket implements Serializable {
+
+    @Id
+    @Column(name = "id")
     private String id;
+
     @NotNull
     @Size(min = 1, max = 60)
+    @Column(name = "title")
     private String title;
+
+    @Column(name = "text")
     private String text;
+
+    @Column(name = "finished")
     private boolean finished;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "created_at")
     private Date createdAt;
 }
--- src/main/java/jp/alhinc/orchestra/ticket/domain/repository/TicketRepository.java
+++ src/main/java/jp/alhinc/orchestra/ticket/domain/repository/TicketRepository.java
@@ -1,13 +1,20 @@
 package jp.alhinc.orchestra.ticket.domain.repository;
 
 import jp.alhinc.orchestra.ticket.domain.model.Ticket;
+import org.springframework.data.domain.Example;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 
-public interface TicketRepository {
+public interface TicketRepository extends JpaRepository<Ticket, String> {
+
     Optional<Ticket> findById(String id);
-    Collection<Ticket> findAll();
+
+    @Override
+    List<Ticket> findAll();
+
     void create(Ticket ticket);
     boolean updateById(Ticket ticket);
     void deleteById(Ticket ticket);
